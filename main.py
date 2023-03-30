ba974af1f0893b9099ddfde46bef2cfc2036db49
urvishp80 <urvishp80@gmail.com>
2023-03-31 01:43:55 +0530

    updated main.py during process someone uploads it will handle that file also

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def start_diarize(whisper_model_name, stemming):
         for audio_file in audio_file_list:
             print(audio_file)
             try:
-                if str(audio_file[:-4] + ".srt") not in srt_file_list:
+                if srt_folder_name+"/"+str(os.path.basename(audio_file)[:-4] + ".srt") not in srt_file_list:
                     audio_file_name = download_file_from_s3(audio_file, bucket, input_folder_name)
                     print(f"Input file name:- {audio_file_name}")
                     if audio_file_name:
@@ -30,8 +30,12 @@ def start_diarize(whisper_model_name, stemming):
 
         srt_file_list = get_all_files_from_s3(bucket, srt_folder_name)
         len_srt_file_list = len(srt_file_list)
+        # Checking during processing any new file uploaded if uploaded it will update len and list
+        audio_file_list = get_all_files_from_s3(bucket, input_folder_name)
+        len_audio_file_list = len(audio_file_list)
 
     print(f"Diarization completed and total {len_srt_file_list} srt files inserted")
+    cleanup(input_data)
 
 
 if __name__ == "__main__":
